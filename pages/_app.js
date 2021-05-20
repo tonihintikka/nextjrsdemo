b03a2a23f56fa2b46f9c3f1940acb112502d79c8
Toni Hintikka <toni.hintikka@gmail.com>
2021-05-20 11:16:10 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+//import '//cdn1.readspeaker.com/script/7474/webReader/webReader.js?pids=wr'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+
+
 
 export default MyApp
